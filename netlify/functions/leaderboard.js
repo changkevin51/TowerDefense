61520b7e5b3a569005fb856094baa7b6a03f5f93
Kevin Chang <changkevin51@gmail.com>
2025-01-19 18:30:40 -0500

    Fix Leaderboard x 4

--- netlify/functions/leaderboard.js
+++ netlify/functions/leaderboard.js
@@ -5,7 +5,9 @@ let sql;
 try {
     sql = neon(process.env.DATABASE_URL, {
         ssl: { rejectUnauthorized: false }
+    
     });
+    console.log('Database connection initialized');
 } catch (error) {
     console.error('Database connection error:', error);
     throw error;
